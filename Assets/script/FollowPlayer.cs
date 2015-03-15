884a098f26959cf8649af39de98568f2b08e7b43
13magician <q631126452@qq.com>
2015-03-15 15:13:55 +0800

    重写代码

---
+++ Assets/script/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform player;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void LateUpdate()
+    {
+        Vector3 v3= player.position;
+        transform.position = new Vector3(v3.x, v3.y, transform.position.z);
+    }
+}
